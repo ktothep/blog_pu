6a4fed46670281e757fabef2fb3612ebfece4ad5
karanprinja <karanprinja@hotmail.com>
2026-03-27 16:17:09 +0530

    Chan ged imports

--- api/index.py
+++ api/index.py
@@ -1,25 +1,21 @@
-import os
-
 try:
     from dotenv import load_dotenv
 except ImportError:
     load_dotenv = lambda: None
+
+load_dotenv()
+
 from fastapi import FastAPI
-from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
-from slowapi.util import get_remote_address
 from slowapi.middleware import SlowAPIMiddleware
 
-from path.tailor import route_tailor
-load_dotenv()
-REDIS_URL = os.getenv("REDIS_URL")
-limiter = Limiter(key_func=get_remote_address, storage_uri=REDIS_URL)
+from api.limiter import limiter
+from api.path.tailor import route_tailor
+
 app = FastAPI(title="Resume Optimizer API", version="1.0")
 
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIMiddleware)
 app.include_router(route_tailor)
-
-
-
